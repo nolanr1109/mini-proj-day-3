e6eef4217397a34dd51121d643d54c40f5ff1355
nolanr1109 <nolanr1109@gmail.com>
2026-02-14 11:05:51 -0600

    added new colors

--- proj3.js
+++ proj3.js
@@ -1,16 +1,26 @@
+
+
 function setup() {
   createCanvas(400, 400);
-    background(10)
+    background(0)
 }
 
 function draw() {
   
   
   noStroke();
-  fill(200, 100, 100)
+  fill(200, 70, 100)
   ellipse(mouseX, mouseY, 25, 25)
 }
 
 function mousePressed(){
   background(0)
+}
+
+function keyPressed(){
+  if (key === 'c'){
+    noStroke();
+    fill(0, 200, 0)
+    ellipse(mouseX, mouseY, 25, 25)
+  }
 }
